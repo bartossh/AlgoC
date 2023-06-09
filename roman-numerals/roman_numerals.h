18913089ae2d1224608dce2585a269cb4087073e
Bartossh <lenart.consulting@gmail.com>
2023-06-09 16:57:04 +0200

    Roman numerals exercism task.

---
+++ roman-numerals/roman_numerals.h
@@ -0,0 +1,8 @@
+#ifndef ROMAN_NUMERALS_H
+#define ROMAN_NUMERALS_H
+
+typedef unsigned int uint32_t;
+char *replace_with(char str[], const char substr[], const char replace[]);
+char *to_roman_numeral(uint32_t number);
+
+#endif
